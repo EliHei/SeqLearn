be80ae85b7b30eee7604cb0bda5ded75b2494205
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-11-26 01:20:22 +0330

    include supplementary packages

---
+++ seqlearner/semisup/__init__.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import keras
+
+from keras.layers import Input, Dense, Dropout, BatchNormalization
+from keras.models import Model
+from keras.callbacks import History, CSVLogger
+
+"""
+    Created by Mohsen Naghipourfar on 11/20/18.
+    Email : mn7697np@gmail.com or naghipourfar@ce.sharif.edu
+    Website: http://ce.sharif.edu/~naghipourfar
+    Github: https://github.com/naghipourfar
+    Skype: mn7697np
+"""
