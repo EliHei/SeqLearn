157f4ef8b060a4fd890cf84e2cc105b970f24f48
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-11-30 18:00:02 +0330

    Add Comments for Freq2Vec

--- seqlearner/Freq2Vec.py
+++ seqlearner/Freq2Vec.py
@@ -7,7 +7,50 @@ from .WordEmbedder import WordEmbedder
 
 
 class Freq2Vec(WordEmbedder):
+    """
+        Freq2Vec Embedding Method. This class is wrapper for Freq2Vec Embedding
+        method to apply on a set of sequences. Child class of WordEmbedder.
+
+        Parameters
+        ----------
+        sequences : numpy ndarray, list, or DataFrame
+           sequences of data like protein sequences
+        word_length : integer
+            The length of each word in sequences to be separated from each other.
+        window_size: integer
+            Size of window for counting the number of neighbors.
+        emb_dim: integer
+            Number of embedding vector dimensions.
+        loss: basestring
+            The loss function is going to be used on training phase.
+        epochs: integer
+            Number of epochs for training the embedding.
+
+        See also
+        --------
+        Freq2Vec.freq2vec_maker : build a model and train it!
+
+    """
     def freq2vec_maker(self):
+        """
+            Train Embedding layer on vocabulary in order to get embedding weights
+            for each word in vocabulary. compress each in `emb_dim` vectors.
+
+            Parameters
+            ----------
+            No parameters are needed.
+
+            Returns
+            -------
+            Nothing will be returned.
+
+            Example
+            --------
+            >>> import pandas as pd
+            >>> sequences = pd.read_csv("./sequences.csv", header=None)
+            >>> freq2vec = Freq2Vec(sequences, word_length=3, window_size=5, emb_dim=25, loss="mean_squared_error", epochs=250)
+            >>> freq2vec.freq2vec_maker()
+        """
         freq2vec = Sequential()
         freq2vec.add(Embedding(input_dim=(len(self.vocab)),
                                output_dim=self.emb_dim,
