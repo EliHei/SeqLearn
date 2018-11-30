fbd88febb587d21cae81e1b8bb029a8f1225c9c5
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-11-30 19:25:18 +0330

    Add Comments for SemiSupervisedLearner

--- seqlearner/SemiSupervisedLearner.py
+++ seqlearner/SemiSupervisedLearner.py
@@ -11,6 +11,34 @@ from .PseudoLabeler import PseudoLabeler
 
 
 class SemiSupervisedLearner:
+    """
+        Wrapper class for semi-supervised learning methods. This class
+        will apply a specific semi-supervised learning method on the
+        sequences and return score for validation sequences.
+
+        Parameters
+        ----------
+        X : list, numpy ndarray, pandas DataFrame
+            list of training embedding vectors for learning
+        y : list, numpy ndarray, pandas DataFrame
+            list of training labels.
+        X_t : list, numpy ndarray, pandas DataFrame
+            list of test embedding vectors for learning
+        y_t : list, numpy ndarray, pandas DataFrame
+            list of test labels.
+
+        Returns
+        -------
+        score : the score of learning model on test data
+
+        See Also
+        -------
+        SemiSupervisedLearner.label_spreading : Apply Label Spreading Algorithm
+        SemiSupervisedLearner.label_propagation : Apply Label Propagation Algorithm
+        SemiSupervisedLearner.naive_bayes : Apply Naive Bayes Algorithm
+        SemiSupervisedLearner.pseudo_labeling : Apply Pseudo Labeling Algorithm
+
+    """
     def __init__(self, X, y, X_t, y_t):
         self.X = X
         self.y = y
@@ -19,6 +47,59 @@ class SemiSupervisedLearner:
 
     def label_spreading(self, kernel='rbf', gamma=20, n_neighbors=7, alpha=0.2,
                         max_iter=30, tol=0.001, n_jobs=1):
+        """
+            LabelSpreading model for semi-supervised learning
+            This model is similar to the basic Label Propagation algorithm,
+            but uses affinity matrix based on the normalized graph Laplacian
+            and soft clamping across the labels.
+
+            Parameters
+            ----------
+            kernel : {'knn', 'rbf', callable}
+                String identifier for kernel function to use or the kernel function
+                itself. Only 'rbf' and 'knn' strings are valid inputs. The function
+                passed should take two inputs, each of shape [n_samples, n_features],
+                and return a [n_samples, n_samples] shaped weight matrix
+
+            gamma : float
+              parameter for rbf kernel
+
+            n_neighbors : integer > 0
+              parameter for knn kernel
+
+            alpha : float
+              Clamping factor. A value in [0, 1] that specifies the relative amount
+              that an instance should adopt the information from its neighbors as
+              opposed to its initial label.
+              alpha=0 means keeping the initial label information; alpha=1 means
+              replacing all initial information.
+
+            max_iter : integer
+              maximum number of iterations allowed
+
+            tol : float
+              Convergence tolerance: threshold to consider the system at steady
+              state
+
+            n_jobs : int or None, optional (default=None)
+                The number of parallel jobs to run.
+                ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+                ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+                for more details.
+
+            Returns
+            -------
+            score : the score of learning model on test data
+
+            Example
+            --------
+            >>> labeled_path = "../data/labeled.csv"
+            >>> unlabeled_path = "../data/unlabeled.csv"
+            >>> mtl = MultiTaskLearner(labeled_path, unlabeled_path)
+            >>> encoding = mtl.embed(word_length=5)
+            >>> X, y, X_t, y_t = train_test_split(mtl.sequences, mtl.labels, test_size=0.33)
+            >>> score = mtl.semi_supervised_learner(X, y, X_t, y_t, ssl="label_spreading")
+        """
         model = LabelSpreading(kernel=kernel, gamma=gamma, n_neighbors=n_neighbors, alpha=alpha,
                                max_iter=max_iter, tol=tol, n_jobs=n_jobs)
         model.fit(self.X, self.y)
@@ -26,6 +107,56 @@ class SemiSupervisedLearner:
 
     def label_propagation(self, kernel='rbf', gamma=20, n_neighbors=7,
                           max_iter=30, tol=1e-3, n_jobs=1):
+        """
+            Label Propagation classifier for semi-supervised learning
+
+            Parameters
+            ----------
+            kernel : {'knn', 'rbf', callable}
+                String identifier for kernel function to use or the kernel function
+                itself. Only 'rbf' and 'knn' strings are valid inputs. The function
+                passed should take two inputs, each of shape [n_samples, n_features],
+                and return a [n_samples, n_samples] shaped weight matrix.
+
+            gamma : float
+                Parameter for rbf kernel
+
+            n_neighbors : integer > 0
+                Parameter for knn kernel
+
+            alpha : float
+                Clamping factor.
+
+                .. deprecated:: 0.19
+                    This parameter will be removed in 0.21.
+                    'alpha' is fixed to zero in 'LabelPropagation'.
+
+            max_iter : integer
+                Change maximum number of iterations allowed
+
+            tol : float
+                Convergence tolerance: threshold to consider the system at steady
+                state
+
+            n_jobs : int or None, optional (default=None)
+                The number of parallel jobs to run.
+                ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+                ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+                for more details.
+
+            Returns
+            -------
+            score : the score of learning model on test data
+
+            Example
+            --------
+            >>> labeled_path = "../data/labeled.csv"
+            >>> unlabeled_path = "../data/unlabeled.csv"
+            >>> mtl = MultiTaskLearner(labeled_path, unlabeled_path)
+            >>> encoding = mtl.embed(word_length=5)
+            >>> X, y, X_t, y_t = train_test_split(mtl.sequences, mtl.labels, test_size=0.33)
+            >>> score = mtl.semi_supervised_learner(X, y, X_t, y_t, ssl="label_propagation")
+        """
         model = LabelPropagation(kernel=kernel, gamma=gamma, n_neighbors=n_neighbors,
                                  max_iter=max_iter, tol=tol, n_jobs=n_jobs)
         model.fit(self.X, self.y)
@@ -33,6 +164,24 @@ class SemiSupervisedLearner:
 
     def naive_bayes(self, distributions=pm.NormalDistribution, verbose=True, max_iter=1e8,
                     stop_threshold=0.1, pseudocount=0, weights=None):
+        """
+            Naive Bayesian algorithm for semi-supervised learning
+
+            Parameters
+            ----------
+            distributions : object, pomegranate object
+                Distribution object from pomegranate package
+            verbose: boolean, default True
+                for showing debug messages
+            max_iter: integer, default 1e8
+                The number of maximum iterations
+            stop_threshold: float, 0.1
+                threshold for stop.
+            pseudocount: integer, default 0
+                //TODO : ?
+            weights: ?
+                ?
+        """
         model = NaiveBayes.from_samples(distributions=distributions, X=self.X, y=self.y, verbose=verbose,
                                         max_iterations=max_iter, stop_threshold=stop_threshold, pseudocount=pseudocount,
                                         weights=weights)
@@ -40,7 +189,20 @@ class SemiSupervisedLearner:
 
     def bayes_classifier(self, distributions=pm.NormalDistribution,
                          max_iter=1e8,
-                         stop_threshold=.1):
+                         stop_threshold=0.1):
+        """
+            Bayesian Classifier for semi-supervised learning
+
+            Parameters
+            ----------
+            distributions : object, pomegranate object
+                Distribution object from pomegranate package
+            max_iter: integer, default 1e8
+                The number of maximum iterations
+            stop_threshold: float, 0.1
+                threshold for stop.
+
+        """
         model = BayesClassifier.from_samples(distributions=distributions, max_iterations=max_iter,
                                              stop_threshold=stop_threshold, X=self.X, y=self.y)
         return model.score(self.X_t, self.y_t)
@@ -49,6 +211,18 @@ class SemiSupervisedLearner:
         pass
 
     def pseudo_labeling(self, alg, sample_rate=0.2):
+        """
+            Pseudo Labeling method for the semi-supervised learning.
+            This method will train a classifier for labeled sequences.
+
+            Parameters
+            ----------
+            alg : object, Sklearn object
+               Sklearn classifier object like svm.SVC classifier.
+            sample_rate: float, default 0.2
+                The proportion of unlabeled sequences from X_t.
+
+        """
         idx = np.where(self.y == -1)[0].tolist()
         X_u = self.X[idx]
         model = PseudoLabeler(model=alg, X_t=X_u, sample_rate=sample_rate)
